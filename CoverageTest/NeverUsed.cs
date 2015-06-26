0edd8486e867948643ff5d844d621586bfaf1714
iain holmes <iain@xamarin.com>
2015-06-26 12:07:13 +0100

    Initial commit

---
+++ CoverageTest/NeverUsed.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace CoverageTest
+{
+	public class NeverUsed
+	{
+		bool ABoolean = false;
+
+		public NeverUsed ()
+		{
+			ABoolean = true;
+		}
+
+		public bool IsThisBiggerThanTen(int n) {
+			if (n > 10) {
+				Console.WriteLine ("{0} is bigger than 10", n);
+				return true;
+			} else {
+				Console.WriteLine ("{0} is not bigger than 10", n);
+				return false;
+			}
+		}
+	}
+}
+
---
+++ CoverageTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace CoverageTest
+{
+	class MainClass
+	{
+		static Random rnd;
+
+		public static void Main (string[] args)
+		{
+			rnd = new Random ();
+			Console.WriteLine ("Hello World!");
+			ALoop (rnd.Next (10, 100));
+			ARandomBranchLoop (rnd.Next (10, 100));
+			NeverFail ();
+		}
+
+		public static void ALoop (int n)
+		{
+			Console.WriteLine ("Looping {0} times", n);
+			for (int i = 0; i < n; i++) {
+				Console.Write ("{0}, ", i);
+			}
+			Console.WriteLine ("");
+		}
+
+		public static void ARandomBranchLoop (int n)
+		{
+			Console.WriteLine ("Random looping {0} times", n);
+			for (int i = 0; i < n; i++) {
+				if (rnd.Next (0, 100) < 50) {
+					Console.Write ("{0}, ", i);
+				} else {
+					Console.Write ("-{0}, ", i);
+				}
+			}
+			Console.WriteLine ("");
+		}
+
+		public static void NeverFail ()
+		{
+			Console.Write ("This will always print true: ");
+			if (true) {
+				Console.WriteLine ("True!");
+			} else {
+				Console.WriteLine ("False!");
+			}
+		}
+	}
+}
